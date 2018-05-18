7b379f676043c8f5c7d5eee06d7861a02d1c9b35
chrzyki <christoph@functor-argument.net>
2018-05-18 12:39:49 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -19,19 +19,19 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld~=4.2',
-        'clldmpg~=3.1',
+        'clld>=4.2.2',
+        'clldmpg>=3.3.1',
         'sqlalchemy',
         'waitress',
     ],
     extras_require={
         'dev': [
             'flake8',
-            'psycopg2',
             'tox',
         ],
         'test': [
             'mock',
+            'psycopg2',
             'pytest>=3.1',
             'pytest-clld',
             'pytest-mock',
