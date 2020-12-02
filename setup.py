9d4dcebf0bcc66d0e139170ab7f76da0100d6fbb
xrotwang <xrotwang@googlemail.com>
2020-12-02 16:20:34 +0100

    upgrade clld, fix data repos

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld>=7',
+        'clld>=7.4',
         'clldmpg>=3.5',
         'pyasjp',
         'sqlalchemy',
